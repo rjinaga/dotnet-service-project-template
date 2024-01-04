5c822e9bf7ec523000f6d457c9365b256f020a01
Rajesh Jinaga <rajes.jinaga@gmail.com>
2024-01-04 12:31:29 +0530

    add few folders to abstractions

---
+++ src/MyService.Abstractions/Contracts/Interface1.cs
@@ -0,0 +1,10 @@
+﻿namespace MyService.Abstractions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal interface Interface1
+{
+}
---
+++ src/MyService.Infrastructure/Database/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace MyService.Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Class1
+{
+}
