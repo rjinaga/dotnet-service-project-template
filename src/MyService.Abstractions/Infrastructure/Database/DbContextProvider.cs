c7d05da6b51ab9cd12c7f30187d3631df08c38c7
Rajesh Jinaga <rajes.jinaga@gmail.com>
2024-01-08 15:58:41 +0530

    Make the GetOrCreateDbContext method threadsafe

--- src/MyService.Abstractions/Infrastructure/Database/DbContextProvider.cs
+++ src/MyService.Abstractions/Infrastructure/Database/DbContextProvider.cs
@@ -4,23 +4,30 @@ public abstract class DbContextProvider
 {
     /// <summary>
     /// Creates new db context instance if it does not associated with <paramref name="context"/> and returns values that's
-    /// associated with context
+    /// associated with context. Its thread safe with given the object <paramref name="scope"/>
     /// </summary>
     /// <param name="context"></param>
     /// <returns></returns>
     public IRelationalDbContext GetOrCreateDbContext(DbContextScope scope)
     {
         ArgumentNullException.ThrowIfNull(nameof(scope));
-        
-        if (scope.GetInternalContext() == null)
+
+        if (scope.GetInternalContext() != null)
         {
-            var relationDbContext = Create(scope);
-            scope.SetInternalContext(relationDbContext);
+            return scope.GetInternalContext()!;
         }
 
+        lock (scope)
+        {
+            if (scope.GetInternalContext() == null)
+            {
+                var relationDbContext = Create(scope);
+                scope.SetInternalContext(relationDbContext);
+            }
+        }
         return scope.GetInternalContext()!;
     }
     protected abstract IRelationalDbContext Create(DbContextScope scope);
-    
+
 }
 
